d4f1a12b42e075c6efb67861d42357b80121a08b
Manuel Bosi <manuelbosi57@gmail.com>
2020-11-23 11:29:22 +0100

    inserimento struttura dati vue

--- js/app.js
+++ js/app.js
@@ -2,6 +2,46 @@
 const app = new Vue({
   el: "#app-container",
   data: {
-    
+
+    // STRUTTURA DATI
+    // Array chatList di oggetti
+    // Tutti gli oggetti avranno le stesse proprietà
+    // immagine, nome, messaggi ricevuti, messaggi inviati, data ultimo messaggio, ultimo messaggio
+    // Messaggi ricevuti: array di stringhe
+    // Messaggi inviati: array di stringhe
+    chatlist: [
+      { 
+      profilePic: "img/avatar_1.jpg", 
+      nome: "Michele", 
+      receivedMessages: ["Ciao", "come ti chiami?"], 
+      sentMessages:["Ciao", "mi chiamo Michele"],
+      lastMessageDate: "20 11 2020 10:52:03",
+      lastMessage: ""
+      },
+      {
+      profilePic: "img/avatar_2.jpg", 
+      nome: "Fabio", 
+      receivedMessages: ["Ciao Fabio", "come stai?"], 
+      sentMessages:["Ciao", "io bene tu?"],
+      lastMessageDate: "20 11 2020 10:52:03",
+      lastMessage: ""
+      },
+      {
+      profilePic: "img/avatar_3.jpg", 
+      nome: "Samuele", 
+      receivedMessages: ["Ciao Samuele", "come stai?"], 
+      sentMessages:["Ciao", "male tu?"],
+      lastMessageDate: "20 11 2020 10:52:03",
+      lastMessage: ""      
+      },
+      {
+      profilePic: "img/avatar_3.jpg", 
+      nome: "Luisa", 
+      receivedMessages: ["Ciao Luisa", "perchè hai i baffi"], 
+      sentMessages:["Ciao", "cosa ti interessa?"],
+      lastMessageDate: "20 11 2020 10:52:03",
+      lastMessage: ""
+      }
+    ]
   }
 });
