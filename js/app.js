84e4bd92321b0fd9589ef08a80fb6c4d6bddb210
Manuel Bosi <manuelbosi57@gmail.com>
2020-11-24 11:00:44 +0100

    inserimento switch chat user bar chat attiva

--- js/app.js
+++ js/app.js
@@ -11,62 +11,64 @@ const app = new Vue({
     // Messaggi inviati: array di stringhe
     chatList: [
       { 
-      profilePic: "img/avatar_1.jpg", 
-      nome: "Michele",
-      lastMessageDate: "01 11 2020 10:52:03",
-      lastMessage: "test michele",
-      messaggi: [ 
-        { testo: "messaggio per Michele", mioMessaggio: true },
-        { testo: "risposta da Michele", mioMessaggio: false } ]
+        profilePic: "img/avatar_1.jpg", 
+        nome: "Michele",
+        lastMessageDate: "01 11 2020 10:52:03",
+        lastMessage: "test michele",
+        messaggi: [ 
+          { testo: "messaggio per Michele", mioMessaggio: true },
+          { testo: "risposta da Michele", mioMessaggio: false }
+        ]
       },
       {
-      profilePic: "img/avatar_2.jpg", 
-      nome: "Fabio",
-      lastMessageDate: "02 11 2020 10:52:03",
-      lastMessage: "test fabio",
-      messaggi: [ 
-        { testo: "messaggio per Fabio", mioMessaggio: true },
-        { testo: "risposta da Fabio", mioMessaggio: false } ]
+        profilePic: "img/avatar_2.jpg", 
+        nome: "Fabio",
+        lastMessageDate: "02 11 2020 10:52:03",
+        lastMessage: "test fabio",
+        messaggi: [ 
+          { testo: "messaggio per Fabio", mioMessaggio: true },
+          { testo: "risposta da Fabio", mioMessaggio: false }
+        ]
       },
       {
-      profilePic: "img/avatar_3.jpg", 
-      nome: "Samuele",
-      lastMessageDate: "03 11 2020 10:52:03",
-      lastMessage: "test samuele",
-      messaggi: [ 
-        { testo: "messaggio per Samuele", mioMessaggio: true },
-        { testo: "risposta da Samuele", mioMessaggio: false } ]      
+        profilePic: "img/avatar_3.jpg", 
+        nome: "Samuele",
+        lastMessageDate: "03 11 2020 10:52:03",
+        lastMessage: "test samuele",
+        messaggi: [ 
+          { testo: "messaggio per Samuele", mioMessaggio: true },
+          { testo: "risposta da Samuele", mioMessaggio: false }
+        ]      
       },
       {
-      profilePic: "img/avatar_4.jpg", 
-      nome: "Luisa",
-      lastMessageDate: "04 11 2020 10:52:03",
-      lastMessage: "test luisa",
-      messaggi: [ 
-        { testo: "messaggio per Luisa", mioMessaggio: true },
-        { testo: "risposta da Luisa", mioMessaggio: false },
-        { testo: "risposta da Luisa Due", mioMessaggio: false } ]
+        profilePic: "img/avatar_4.jpg", 
+        nome: "Luisa",
+        lastMessageDate: "04 11 2020 10:52:03",
+        lastMessage: "test luisa",
+        messaggi: [ 
+          { testo: "messaggio per Luisa", mioMessaggio: true },
+          { testo: "risposta da Luisa", mioMessaggio: false },
+          { testo: "risposta da Luisa Due", mioMessaggio: false }
+        ]
       }
     ],
-    activeUser: "Clicca una chat per visualizzare i messaggi",
-    activePic: "img/avatar_8.jpg",
-    activeLastMsgDate: "",
     isActiveChat: false,
-    indexActive: 0
+    indexActive: 5 // altrimenti mi seleziona di default index 0
   },
   methods: {
     // Al click su ogni chat cambia la chat corrente
     changeChat(i) {
-      console.log("INDICE ELEMENTO CLICCATO", i);
+      console.log("INDEX", i);
       
       // CAMBIO VALORI USERBAR
       this.activeUser = this.chatList[i].nome;
       this.activePic = this.chatList[i].profilePic;
       this.activeLastMsgDate = this.chatList[i].lastMessageDate;
       // Cambio variabile per mostrare schermata chat cliccata
-      // this.isActiveChat = true;
+      this.isActiveChat = true;
 
       // Cambio l'indice attivo
+      // e assegno il valore di indexChat riga 60 html
       this.indexActive = i
       
     }
