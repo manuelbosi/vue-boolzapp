e78647f4f277a2a6f0bcae66abf958ad1d4146e2
Manuel Bosi <manuelbosi57@gmail.com>
2020-11-24 18:11:26 +0100

    creata cartella bonus inserito scroll automatico

---
+++ bonus/js/app.js
@@ -0,0 +1,113 @@
+// CREAZIONE ISTANZA VUE JS
+const app = new Vue({
+  el: "#app-container",
+  data: {
+
+    // STRUTTURA DATI
+    // Array chatList di oggetti
+    // Tutti gli oggetti avranno le stesse proprietà
+    // immagine, nome, messaggi ricevuti, messaggi inviati, data ultimo messaggio, ultimo messaggio
+    // Messaggi ricevuti: array di stringhe
+    // Messaggi inviati: array di stringhe
+    chatList: [
+      { 
+        profilePic: "img/avatar_1.jpg", 
+        nome: "Michele",
+        messaggi: [ 
+          { testo: "messaggio per Michele", mioMessaggio: true, date: "01/01/2020 15:30:55" },
+          { testo: "risposta da Michele", mioMessaggio: false, date: "02/01/2020 15:30:55"}
+        ]
+      },
+      {
+        profilePic: "img/avatar_2.jpg", 
+        nome: "Fabio",
+        messaggi: [ 
+          { testo: "messaggio per Fabio", mioMessaggio: true, date: "03/01/2020 15:30:55" },
+          { testo: "risposta da Fabio", mioMessaggio: false, date: "04/01/2020 15:30:55" }
+        ]
+      },
+      {
+        profilePic: "img/avatar_3.jpg", 
+        nome: "Samuele",
+        messaggi: [ 
+          { testo: "messaggio per Samuele", mioMessaggio: true, date: "05/01/2020 15:30:55" },
+          { testo: "risposta da Samuele", mioMessaggio: false, date: "06/01/2020 15:30:55" }
+        ]      
+      },
+      {
+        profilePic: "img/avatar_4.jpg", 
+        nome: "Luisa",
+        messaggi: [ 
+          { testo: "messaggio per Luisa", mioMessaggio: true, date: "07/01/2020 10:30:55" },
+          { testo: "risposta da Luisa", mioMessaggio: false, date: "08/01/2020 15:30:55" },
+          { testo: "risposta da Luisa Due", mioMessaggio: false, date: "25/01/2020 15:30:55" },
+          { testo: "messaggio per Luisa Due", mioMessaggio: true, date: "31/01/2020 10:30:55" }
+        ]
+      }
+    ],
+    isActiveChat: false,
+    indexActive: 5, // altrimenti mi seleziona di default index 0
+    yourMessage: ""
+  },
+  methods: {
+    // Al click su ogni chat cambia la chat corrente
+    changeChat(i) {
+      console.log("INDEX", i);
+      
+      // CAMBIO VALORI USERBAR
+      this.activeUser = this.chatList[i].nome;
+      this.activePic = this.chatList[i].profilePic;
+      this.activeLastMsgDate = this.chatList[i].lastMessageDate;
+      // Cambio variabile per mostrare schermata chat cliccata
+      this.isActiveChat = true;
+
+      // Cambio l'indice attivo
+      // e assegno il valore di indexChat riga 60 html
+      this.indexActive = i
+      // console.log(this.chatList[this.indexActive].messaggi[this.chatList[this.indexActive].messaggi.length-1].date);
+    },
+    sendMessage() {
+
+      // Creo un nuovo oggetto messaggio da inserire
+      let newMessage = {
+        testo: this.yourMessage,
+        mioMessaggio: true,
+        date: this.getCurrentDate()
+      };
+
+      // Pusho il nuovo messaggio nella chat attiva
+      this.chatList[this.indexActive].messaggi.push(newMessage);
+
+      // Ripulisco l'input
+      this.yourMessage = "";
+
+      let autoReponse = {
+        testo: "risposta automatica",
+        mioMessaggio: false,
+        date: this.getCurrentDate()
+      }
+
+      // Faccio partire un timer di 1 secondo che esegue la funzione
+      // Con la funziona normale non va con l'arrow function sì
+      setTimeout(() => {
+        // Pusho la risposta automatica nella chat attiva
+        this.chatList[this.indexActive].messaggi.push(autoReponse);
+      }, 1000);
+    },
+    getCurrentDate() {
+      let date = new Date();
+      let day = date.getDate();
+      let month = date.getMonth();
+      let year = date.getFullYear();
+      let hours = date.getHours();
+      let minutes = date.getMinutes();
+      let seconds = date.getSeconds();
+      return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`
+    }
+  },
+  updated: function () {
+    var container = document.querySelector(".current-chat");
+    var scrollHeight = container.scrollHeight;
+    container.scrollTop = scrollHeight;
+  }
+});
